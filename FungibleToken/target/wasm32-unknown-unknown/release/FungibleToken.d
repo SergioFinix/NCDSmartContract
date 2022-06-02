1375783ea280c446a072795596481bb3cf0031b5
Do0k13 <ludim.chavez.estevez@gmail.com>
2022-06-01 22:37:31 -0500

    nuevo archivo:  Cargo.lock 	nuevo archivo:  Cargo.toml 	nuevo archivo:  res/FungibleToken.wasm 	nuevo archivo:  src/main.rs 	nuevo archivo:  target/.rustc_info.json 	nuevo archivo:  target/CACHEDIR.TAG 	nuevo archivo:  target/release/.cargo-lock 	nuevo archivo:  target/wasm32-unknown-unknown/CACHEDIR.TAG 	nuevo archivo:  target/wasm32-unknown-unknown/release/.cargo-lock 	nuevo archivo:  target/wasm32-unknown-unknown/release/.fingerprint/FungibleToken-ce827d220197217d/bin-FungibleToken 	nuevo archivo:  target/wasm32-unknown-unknown/release/.fingerprint/FungibleToken-ce827d220197217d/bin-FungibleToken.json 	nuevo archivo:  target/wasm32-unknown-unknown/release/.fingerprint/FungibleToken-ce827d220197217d/dep-bin-FungibleToken 	nuevo archivo:  target/wasm32-unknown-unknown/release/.fingerprint/FungibleToken-ce827d220197217d/invoked.timestamp 	nuevo archivo:  target/wasm32-unknown-unknown/release/.fingerprint/FungibleToken-ce827d220197217d/output-bin-FungibleToken 	nuevo archivo:  target/wasm32-unknown-unknown/release/FungibleToken.d 	nuevo archivo:  target/wasm32-unknown-unknown/release/FungibleToken.wasm 	nuevo archivo:  target/wasm32-unknown-unknown/release/deps/FungibleToken-ce827d220197217d.d 	nuevo archivo:  target/wasm32-unknown-unknown/release/deps/FungibleToken-ce827d220197217d.wasm

---
+++ FungibleToken/target/wasm32-unknown-unknown/release/FungibleToken.d
@@ -0,0 +1,1 @@
+/home/lchavez/NCDSmartContract/FungibleToken/target/wasm32-unknown-unknown/release/FungibleToken.wasm: /home/lchavez/NCDSmartContract/FungibleToken/src/main.rs
---
+++ FungibleToken/target/wasm32-unknown-unknown/release/deps/FungibleToken-ce827d220197217d.d
@@ -0,0 +1,5 @@
+/home/lchavez/NCDSmartContract/FungibleToken/target/wasm32-unknown-unknown/release/deps/FungibleToken-ce827d220197217d.wasm: src/main.rs
+
+/home/lchavez/NCDSmartContract/FungibleToken/target/wasm32-unknown-unknown/release/deps/FungibleToken-ce827d220197217d.d: src/main.rs
+
+src/main.rs:
